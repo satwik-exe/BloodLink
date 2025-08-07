daf6437efe268705550437c36786f85f0e8b4e53
Satwik Mohanty <satwikmohanty69@gmail.com>
2025-08-07 13:33:34 +0530

    Initial setup(Vite+React+TypeScript+Tailwindcss)

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600 mb-4">Tailwind is Working! 🎉</h1>
+      <button className="px-4 py-2 bg-green-500 hover:bg-green-600 text-white rounded">
+        Click Me
+      </button>
+    </div>
+  );
+}
+
+export default App;
+
